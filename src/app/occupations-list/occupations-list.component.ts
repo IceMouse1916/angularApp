5e9d4f0f4db90898b58b7a326d8b36b3a0499745
nkamaszewski <norbert.kamaszewski@gmail.com>
2018-01-17 15:37:34 +0100

    recommended and favourite labels functionality added

--- src/app/occupations-list/occupations-list.component.ts
+++ src/app/occupations-list/occupations-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { UsersService } from '../users.service';
+
 @Component({
   selector: 'nk-occupations-list',
   templateUrl: './occupations-list.component.html',
@@ -8,11 +10,31 @@ import { Component, OnInit, Input } from '@angular/core';
 export class OccupationsListComponent implements OnInit {
 
   @Input()
-  nkDataToRender:String;
+  nkDataToRender;
 
-  constructor() { }
 
+  data;
+  recommended;
+
+  constructor(private user: UsersService) { }
+  
   ngOnInit() {
+    this.data = this.user.getfavourites();
+    this.recommended = this.user.getrecommended();
   }
 
+
+  isFavouirite(id){
+    for(let index in this.data){
+      if(this.data[index].id === id)
+        return true;
+    }
+  }
+
+  isRecommended(id){
+    for(let index in this.recommended){
+      if(this.recommended[index].id === id)
+        return true;
+    }
+  }
 }
